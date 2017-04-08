b53dbf56a906f45af8747f30058d11bce672ff58
ertigi <ertigi66@gmail.com>
2017-04-08 13:44:28 +0300

    Create laba_1.3.c

---
+++ laba_1.3.c
@@ -0,0 +1,19 @@
+//Вариант №3
+//Определить, является ли заданное число N полным квадратом
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+#define N 100
+
+int main()
+{
+	setlocale(LC_ALL, "en_US.UTF-8");
+
+	int c;
+	float n;
+	n = N;
+	c = '√';
+	printf("%lcn = %f", 251, sqrtf(n));
+	getchar();
+}
